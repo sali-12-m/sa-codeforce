97a448615ac1df0b052e02db1ca6553c7bdf7b74
sali-12-m <salimmoh870@gmail.com>
2026-01-19 18:58:53 +0300

    Create odd set.py

---
+++ odd set.py
@@ -0,0 +1,15 @@
+int(input())
+for j in range(t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    odd=0
+    even=0
+    for i in a:
+        if i%2==0:
+            even+=1
+        else:
+            odd+=1
+    if even == odd ==n:
+        print("Yes")
+    else:
+        print("No")
